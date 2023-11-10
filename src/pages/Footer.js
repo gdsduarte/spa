480d247e82d77a8cf3443db4af5e5e1791e83f86
gdsduarte <gds_duarte@hotmail.com>
2023-11-10 21:19:26 +0000

    Refactor code for improved readability and consistency

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,25 +1,26 @@
-
 const Footer = () => {
-    
   return (
-      <footer className="theme-dark footer-0 text-center-desktop text-center-mobile">
-  
-          <h2>ABC Limited</h2>
-          <h3>Awesome and affordable products</h3>
-  
-          <ul className="footer-links">
-              <li><a href="/">Home</a></li>
-              <li><a href="/products">Products</a></li>
-              <li><a href="/services">Services</a></li>
-          </ul>
-  
-  
-          <div className="copyright">
-              <p>Copyright © 2023 ABC Limited. All rights reserved.</p>
-          </div>
-      </footer>
- 
-  )
-}
+    <footer className="theme-dark footer-0 text-center-desktop text-center-mobile">
+      <h2>ABC Limited</h2>
+      <h3>Awesome and affordable products</h3>
+
+      <ul className="footer-links">
+        <li>
+          <a href="/spa">Home</a>
+        </li>
+        <li>
+          <a href="/products">Products</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+      </ul>
+
+      <div className="copyright">
+        <p>Copyright © 2023 ABC Limited. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,23 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
   return (
     <>
       <header className="theme-light text-center-desktop text-center-mobile">
-
-      <div className="container-text">
+        <div className="container-text">
           <h1>Page not found</h1>
-      </div>
+        </div>
+      </header>
 
-    </header>
-
-    <section class="w-820px">
-
+      <section class="w-820px">
         <p>Sorry, this page does not exist.</p>
-        <p>Return to the website <a href="/">Home</a> page.</p>
+        <p>
+          Return to the website <a href="/">Home</a> page.
+        </p>
 
-        <p>Return to the website <Link to="/">Home</Link> page.</p>
-    </section>
+        <p>
+          Return to the website <Link to="/">Home</Link> page.
+        </p>
+      </section>
     </>
   );
-}
+};
